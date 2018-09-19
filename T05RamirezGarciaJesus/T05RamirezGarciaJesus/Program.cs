4e59eceeb4d6d5289bf1733dd90db084207957bf
jrg131298 <42877387+jrg131298@users.noreply.github.com>
2018-09-19 12:52:52 -0700

    T05RamirezGarciaJesus
    Estructura de datos

---
+++ T05RamirezGarciaJesus/T05RamirezGarciaJesus/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace T05RamirezGarciaJesus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch tiempo = Stopwatch.StartNew();
+            Int32 suma = 0;
+            for (Int32 cont = 1; cont <= 1000000; cont++)
+            {
+                suma = suma + cont;
+            }
+
+            Console.WriteLine("El resultado es: {0}", suma);
+            Console.WriteLine("El tiempo requerido fue: {0}", tiempo.Elapsed);
+            Console.WriteLine("Preciona una tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
